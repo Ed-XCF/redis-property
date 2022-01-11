4568e17fa98c94a83c21230d7fae5353416c982b
Ed-XCF <m.tofu@qq.com>
2022-01-11 22:05:55 +0800

    init repo

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    install_requires = fh.read().splitlines()
+
+with open("version", "r") as fh:
+    version = fh.read()
+
+setuptools.setup(
+    name="redis-property",
+    version=version,
+    author="Ed__xu__Ed",
+    author_email="m.tofu@qq.com",
+    description="A decorator for caching properties in Redis.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ed-XCF/redis-property",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=install_requires,
+)
