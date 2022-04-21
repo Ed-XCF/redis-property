5d472d1f172878902f731414893ca445500a8711
Ed-XCF <m.tofu@qq.com>
2022-04-21 17:47:04 +0800

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # redis-property
-A decorator for caching properties in Redis.
+A decorator for caching properties in redis. Inspired from [cached-property](https://github.com/pydanny/cached-property)
+
+## Installation
+```shell
+pip3 install redis-property
+```
+
+## How to use it
+
+Setup redis_property
+
+```python
+import redis_property
+
+redis_property.configure(
+    "redis://:111@wow:6379/0", 
+    lambda o, f: f"{type(o).__name__}:{o.id}:{f.__name__}"
+)
+```
+
+use it like python property
+
+```python
+from redis_property import redis_property
+
+class Something:
+    id = 1
+    
+    @redis_property(seconds=10)  # 24h by default
+    def name(self):
+        return 1
+
+something = Something()
+print(something.name)
+```
+
+invalidating the cache
+
+```python
+del something.name
+```
+
+use it with cached-property
+
+```python
+from cached_property import cached_property
+from redis_property import redis_property
+
+class Something:
+    id = 1
+    
+    @cached_property
+    @redis_property
+    def name(self):
+        return 1
+```
---
+++ redis_property.py
@@ -0,0 +1,114 @@
+import json
+from threading import RLock
+from typing import Callable, Optional
+
+from redis import Redis, RedisError
+
+__all__ = ["redis_property"]
+
+_redis_cli: Optional[Redis] = None
+
+
+def _default_key(obj, func):
+    return type(obj).__name__ + func.__name__
+
+
+def configure(url: str, default_key: Callable = None):
+    global _redis_cli
+    _redis_cli = Redis.from_url(url)
+
+    if default_key is not None:
+        global _default_key  # noqa
+        _default_key = default_key
+
+
+def assert_redis_cli_exists():
+    assert _redis_cli is not None, "Redis url has not been configured"
+
+
+def safe_read(key):
+    try:
+        value: bytes = _redis_cli.get(key)
+    except RedisError:
+        return
+    else:
+        if value is None:
+            return
+
+        return value.decode("utf-8")
+
+
+def safe_write(key, value, ttl):
+    try:
+        return _redis_cli.set(key, value, ex=ttl, nx=True)
+    except RedisError:
+        return
+
+
+def safe_remove(key):
+    try:
+        return _redis_cli.delete(key)
+    except RedisError:
+        return
+
+
+class redis_property:  # noqa
+    def __init__(self, seconds=24 * 60 * 60, key=None):
+        assert_redis_cli_exists()
+
+        if callable(seconds):
+            self.func, self.ttl = seconds, None
+        else:
+            self.func, self.ttl = None, seconds
+
+        self._key = key or _default_key
+        self._copy_func_info()
+        self.lock = RLock()
+
+    def __call__(self, func):
+        if self.func is not None:
+            return self.__get__(func, None)
+
+        self.func = func
+        self._copy_func_info()
+        return self
+
+    def _copy_func_info(self):
+        if self.func is None:
+            return
+
+        for member_name in [
+            "__doc__",
+            "__name__",
+            "__module__",
+        ]:
+            value = getattr(self.func, member_name)
+            setattr(self, member_name, value)
+
+    def __get__(self, instance, _):
+        if instance is None:
+            return self
+
+        key = self._make_key(instance)
+        value = safe_read(key)
+        if value is not None:
+            return self._loads(value)
+
+        with self.lock:
+            value = safe_read(key)
+            if value is not None:
+                return self._loads(value)
+
+            value = self.func(instance)
+            safe_write(key, self._dumps(value), self.ttl)
+            return value
+
+    def __delete__(self, instance):
+        key = self._make_key(instance)
+        safe_remove(key)
+
+    _dumps = staticmethod(json.dumps)
+    _loads = staticmethod(json.loads)
+
+    def _make_key(self, obj):
+        return str(self._key(obj, self.func) if callable(self._key) else self._key)
