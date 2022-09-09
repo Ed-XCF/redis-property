d364873276bf77e0e5801b4e5c91833a1f6e049e
Ed-XCF <m.tofu@qq.com>
2022-09-09 11:27:36 +0800

    add switch

--- test_redis_property.py
+++ test_redis_property.py
@@ -5,13 +5,14 @@ from cached_property import cached_property
 import redis_property as redis_property_lib
 from redis_property import redis_property
 
-redis_property_lib.configure(
-    "redis://:@127.0.0.1:6379/0",
-    lambda o, f: f"{type(o).__name__}:{o.id}:{f.__name__}"
-)
-
 
 class TestRedisProperty:
+    def setUp(self):
+        redis_property_lib.configure(
+            "redis://:@127.0.0.1:6379/0",
+            lambda o, f: f"{type(o).__name__}:{o.id}:{f.__name__}"
+        )
+
     def test_work_alone(self):
         class Adder:
             id = random.randint(1, 1000)
@@ -28,6 +29,49 @@ class TestRedisProperty:
         assert adder.number == adder.number
         del adder.number
         assert adder.number == 3
+        del adder.number
+
+    def test_work_with_cached_property(self):
+        class Adder:
+            id = random.randint(1, 1000)
+
+            def __init__(self):
+                self.times = 1
+
+            @cached_property
+            @redis_property(1)
+            def number(self):
+                self.times += 1
+                return self.times
+
+        adder = Adder()
+        assert adder.number == adder.number
+
+
+class TestDisabledRedisProperty:
+    def setUp(self):
+        redis_property_lib.configure(
+            "redis://:@127.0.0.1:6379/0",
+            lambda o, f: f"{type(o).__name__}:{o.id}:{f.__name__}",
+            disable=True,
+        )
+
+    def test_work_alone(self):
+        class Adder:
+            id = random.randint(1, 1000)
+
+            def __init__(self):
+                self.times = 1
+
+            @redis_property(1)
+            def number(self):
+                self.times += 1
+                return self.times
+
+        adder = Adder()
+        assert adder.number != adder.number
+        del adder.number
+        assert adder.number == 4
         del adder.number
 
     def test_work_with_cached_property(self):
