d2d497466cb8dfcab03297a5991d9e9b9969c718
Ed-XCF <m.tofu@qq.com>
2022-11-29 18:52:42 +0800

    update py version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=install_requires,
     include_package_data=True,
 )
